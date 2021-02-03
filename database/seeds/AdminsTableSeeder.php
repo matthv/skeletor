efa6f1e44dc107f6cd509dffbd984b2a78c39d6a
Matthieu <matthv@gmail.com>
2021-02-03 10:20:20 +0100

    feat(seed): update

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -13,23 +13,11 @@ class AdminsTableSeeder extends Seeder
      */
     public function run()
     {
-    	// ROLES
-    	$roles = ['super_admin', 'admin'];
-    	foreach($roles as $r) {
-			$role = new \Matthv\Skeletor\Models\DashboardRole();
-			$role->name = $r;
-			$role->save();
-		}
-
-		$role_superadmin 	= \Matthv\Skeletor\Models\DashboardRole::where('name', 'super_admin')->first();
-		$role_admin 		= \Matthv\Skeletor\Models\DashboardRole::where('name', 'admin')->first();
-
 		// USERS
 		$admin = new \Matthv\Skeletor\Models\Admin();
 		$admin->name 	= 'Admin';
 		$admin->email 	= 'admin@skeletor.com';
         $admin->password = bcrypt('admin');
 		$admin->save();
-		$admin->roles()->attach([$role_superadmin->id, $role_admin->id]);
 	}
 }
